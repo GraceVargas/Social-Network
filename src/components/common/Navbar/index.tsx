892872c91f7add093f210a0762583ab3c034e1ba
Grace Vargas <gracielavargasg@gmail.com>
2022-09-03 18:46:51 -0300

    create navbar

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,5 +1,76 @@
+import React from "react";
+import { BrowserRouter, NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg" id="navbar">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" to={"/"}>
+              ConectADAs
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink
+                    to={"/"}
+                    className="nav-link active"
+                    aria-current="page"
+                  >
+                    Iniciar sesión
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to={"/"}
+                    className="nav-link active"
+                    aria-current="page"
+                  >
+                    Registrarse{" "}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to={"/"}
+                    className="nav-link active"
+                    aria-current="page"
+                  >
+                    Cerrar sesión
+                  </NavLink>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Buscar amigos"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export { Navbar };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./assets/styles.css";
+import { Home } from "./pages/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(<Home />);
