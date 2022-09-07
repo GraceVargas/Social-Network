e829a26d02c8fcfe0da3f5c7d7901bef2b72ada3
Grace Vargas <gracielavargasg@gmail.com>
2022-09-06 22:18:47 -0300

    add react bootstrap and navbar component

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,79 +1,39 @@
-import React from "react";
-import { BrowserRouter, NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-const Navbar = () => {
+const NavScrollExample = () => {
   return (
-    <>
-      <BrowserRouter>
-        <nav
-          className="navbar navbar-expand-lg navbar navbar-dark bg-dark"
-          id="navbar"
-        >
-          <div className="container-fluid">
-            <NavLink className="navbar-brand" to={"/"}>
-              ConectADAs
-            </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <NavLink
-                    to={"/login"}
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Ingresar
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink
-                    to={"/signup"}
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Registrarse
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink
-                    to={"/logout"}
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Cerrar sesión
-                  </NavLink>
-                </li>
-              </ul>
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Buscar amigos"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline btn-dark" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
-      </BrowserRouter>
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">ConectADAs</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/login">Ingresar</Nav.Link>
+            <Nav.Link href="/signup">Registrarse</Nav.Link>
+            <Nav.Link href="/logout">Cerrar sesión</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar amigos"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="dark">Buscar</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export { Navbar };
+export { NavScrollExample as Navbar };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/styles.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { Home } from "./pages/Home";
 
 const root = ReactDOM.createRoot(
