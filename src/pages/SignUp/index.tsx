759cb7adb48caf9f593538d7fae39c37b7b689cc
Grace Vargas <gracielavargasg@gmail.com>
2022-09-11 20:09:23 -0300

    add sign up page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "../../components/common";
+
+const Signup = () => {
+  return (
+    <>
+      <Layout hideNav page="signup">
+        <div>Sign Up</div>
+      </Layout>
+    </>
+  );
+};
+
+export { Signup };
