9ed55d764158d3b4a881531f95b7a64153fd0497
Grace Vargas <gracielavargasg@gmail.com>
2022-10-10 00:49:31 -0300

    add grid, cards and style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,49 @@
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import { Layout } from "../../components/common";
 import { withAuth } from "../../hoc";
+import "./styles.scss";
 
 const HomePage = () => {
   return (
     <>
       <Layout page="home">
-        <div>Home</div>
+        <Container>
+          <Row className="py-3">
+            <Col className="col-home">
+              <aside>
+                <Card className="card-home" bg="dark" text="white">
+                  <Card.Body>
+                    <Card.Title>Usuarios que seguís</Card.Title>
+                  </Card.Body>
+                </Card>
+                <Card className="card-home" bg="dark" text="white">
+                  <Card.Body>
+                    <Card.Title>Usuarios que aún no seguís</Card.Title>
+                  </Card.Body>
+                </Card>
+              </aside>
+            </Col>
+            <Col className="col-home" xs={9}>
+              <div>
+                <Card className="card-home" bg="dark" text="white">
+                  <Card.Body>
+                    <Form className="p-2  ">
+                      <Form.Label>¿Qué te gustaría compartirnos?</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Ingresá el título de tu publicación"
+                      />
+                      <Form.Control as="textarea" aria-label="With textarea" />
+                      <Button variant="primary" type="submit">
+                        Publicar
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Layout>
     </>
   );
